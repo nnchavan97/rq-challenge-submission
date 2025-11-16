89902a1f9df422d0e78545abb845d34532d5f86d
nnchavan97 <nn.chavan97@gmail.com>
2025-11-16 13:07:13 +0530

    Employee API interface doc changes

--- api/src/main/java/com/reliaquest/api/service/IEmployeeService.java
+++ api/src/main/java/com/reliaquest/api/service/IEmployeeService.java
@@ -11,17 +11,49 @@ import java.util.List;
  */
 public interface IEmployeeService {
 
+    /**
+     * Retrieves all employees.
+     */
     List<EmployeeDto> getAllEmployees();
 
+    /**
+     * Retrieves an employee by ID.
+     *
+     * @param id employee ID
+     * @return EmployeeDto containing employee details if found
+     */
     EmployeeDto getEmployeeById(String id);
 
+    /**
+     * Creates a new employee.
+     *
+     * @param employeeRequestDto employee creation input
+     * @return created employee details
+     */
     EmployeeDto createEmployee(CreateEmployeeRequestDto employeeRequestDto);
 
+    /**
+     * Deletes an employee by ID.
+     *
+     * @param id employee ID
+     * @return Employee Name if deletion succeeded
+     */
     String deleteEmployeeById(String id);
 
+    /**
+     * Finds employees with matching names.
+     *
+     * @param name name keyword
+     */
     List<EmployeeDto> searchEmployeesByName(String name);
 
+    /**
+     * Gets the highest salary among employees.
+     */
     Integer getHighestSalaryOfEmployees();
 
+    /**
+     * Gets the names of the top ten highest-earning employees.
+     */
     List<String> getTopTenHighestEarningEmployeeNames();
 }
